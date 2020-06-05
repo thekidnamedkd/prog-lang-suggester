2a8ae7973ea12688f1d6e57b25f6b84919febb11
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-05 11:33:08 -0700

    repeat parse structure for q1-q7 and console.log to test/debug functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,20 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event){
     event.preventDefault();
 
-    const q1 = parseInt($('input[name="q1"]:checked').val())
+    const q1 = parseInt($('input[name="q1"]:checked').val());
+    const q2 = parseInt($('input[name="q2"]:checked').val());
+    const q3 = parseInt($('input[name="q3"]:checked').val());
+    const q4 = parseInt($('input[name="q4"]:checked').val());
+    const q5 = parseInt($('input[name="q5"]:checked').val());
+    const q6 = parseInt($('input[name="q6"]:checked').val());
+    const q7 = parseInt($('input[name="q7"]:checked').val());
+
     console.log(q1)
+    console.log(q2)
+    console.log(q3)
+    console.log(q4)
+    console.log(q5)
+    console.log(q6)
+    console.log(q7)
 });
 })
